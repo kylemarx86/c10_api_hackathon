34bbfc6bbf98d7b863547d3ee60524a9e1b81a4e
Collette-Tamez <cttmz4@gmail.com>
2016-09-21 15:38:15 -0700

    commented function

--- script.js
+++ script.js
@@ -3,41 +3,82 @@
  * makeFirstAjaxCall - makes a request to The Movie DB to return search results via AJAX
  */
 function makeAjaxCall() {
-    console.log("BUTTON CLICKED");
-    console.log("FUNCTION START");
-    var dataToSendServer = {
+    /**
+     * dataToSendServerForFirstCall - local variable that holds the data to send to The Movie DB database API in the first AJAX call
+     * @type {{api_key: string, query: string}}
+     */
+    var dataToSendServerForFirstCall = {
         api_key: "7e73e5037ed00682f5aae1e5b6d940a4",
         query: "star wars vii"
     };
-    var apiKey = {
+    /**
+     * dataToSendServerForSecondCall - local variable that holds the data to send to The Movie DB database in the second AJAX call
+     * @type {{api_key: string}}
+     */
+    var dataToSendServerForSecondCall  = {
         api_key: "7e73e5037ed00682f5aae1e5b6d940a4" // for second ajax call
     };
-    console.log("BEGIN AJAX");
+    /**
+     * AJAX call to The Movie Database API that performs a search query based on the keyword variable
+     */
     $.ajax({
-        data: dataToSendServer,
+        data: dataToSendServerForFirstCall,
         dataType: "JSON",
         method: "GET",
         url: "https://api.themoviedb.org/3/search/movie?",
+        /**
+         * anonymous error function - lets the user know their search was invalid
+         * @param response
+         */
         error: function (response) {
-            console.log("THERE WAS AN ERROR: " + response);
+            $("<h1>").text("We couldn't find anything!").appendTo("#divForQuote");
         }
+        /**
+         * anonymous success function - executes on success of first ajax call?
+         * @param response
+         */
     }).then(function (response) {
+        /**
+         * result - local variable that stores the object at index zero of the key "results" array in response object
+         */
         var result = response.results[0];
-        console.log(result);
+        /**
+         * movieID - local variable that stores the value of the key id in the results variable
+         */
         var movieID = result.id;
+        /**
+         * url - stores the url to be sent to the data base that includes the necessary path parameter
+         * @type {string}
+         */
         var url = 'https://api.themoviedb.org/3/movie/' + movieID + '?';
+        /**
+         * ajax call to The Movie DB API to retrieve detailed movie information
+         */
         $.ajax({
-            data: apiKey,
+            data: dataToSendServerForSecondCall,
             dataType: "JSON",
             method: "GET",
             url: url,
+            /**
+             * anonymous error function - lets the user know there was an error
+             * @param response
+             */
             error: function (response) {
-                console.log("THERE WAS AN ERROR: " + response);
+                $("<h1>").text("We couldn't find anything!").appendTo("#divForQuote");
             },
+            /**
+             * anonymous success function - on successful request appends selected information to DOM
+             * @param response
+             */
             success: function (response) {
+                /**
+                 * result - local variable that stores the response object
+                 */
                 var movieData = response;
-                console.log("success!");
-                console.log(movieData);
+                /**
+                 * moviePoster - local variable that concats URL needed to resolve a TMDB image and the backdrop_path image file path in response object
+                 * @type {string}
+                 */
                 var moviePoster = "http://image.tmdb.org/t/p/original" + movieData.backdrop_path;
                 $("body").css('background-image', 'url(' + moviePoster + ')');
                 $("<h1>").text(movieData.original_title).appendTo("#divForQuote");
@@ -50,42 +91,14 @@ function makeAjaxCall() {
 }
 
 /**
- * call addClickHandlers on page load
+ * Listen for the document to load and calls addClickHandlers function
  */
 $(document).ready(function () {
     addClickHandlers();
-    console.log("PAGE LOADED")
 });
 /**
- * addClickHandlers - and click handler functions to dom elements
+ * addClickHandlers - and click handler functions to button in DOM with id of movieInfo
  */
 function addClickHandlers() {
     $("#movieInfo").click(makeAjaxCall);
 }
-
-////TODO: HOLD api-key => key AS ONE VARIABLE?
-////TODO: PATH PARAMETERS?
-
-// ajax call to movie quotes api and subsequent ajax call to omdb
-$.ajax({
-  type: "POST",
-  headers: {
-    "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V"
-  },
-  dataType: 'json',
-  url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies'      
-}).then(function(res) {
-  console.log(res);
-  var quote = res.quote;
-  var author = res.author;
-  var endPoint = 'http://www.omdbapi.com/?t=' + author + '&y=&plot=short&r=json';
-  console.log(quote + ' - ' + author);
-  return $.ajax({
-    type: "GET",
-    url: endPoint
-  });
-}).then(function(res) {
-  var plot = res.Plot;
-  if (res.Plot == undefined || res.Plot == 'N/A') plot = "plot not found";
-  console.log(plot);
-});
