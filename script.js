a7798b34d2953c4c1a88cd24703cbe97d1ae36dc
Kyle Marx <Kyle Marx>
2016-09-21 16:25:40 -0700

    removed intermediate funciton that call the ajax call and iframe update. added comments

--- script.js
+++ script.js
@@ -15,26 +15,22 @@ $(document).ready(function () {
  */
 function addEventHandlers() {
     $('button').click(function () {
-        updateVideo();
+        updateMovieTrailerByKeyword('the terminator 1');
     });
 }
-
-
-
-function updateVideo() {
-    searchYouTubeByKeyword('star wars episode vii');
-}
-
+//
+// function updateVideo() {
+//     searchYouTubeByKeyword('the fast and the furious');
+// }
 
 /**
- * This function searches for an video official movie trailer for a given movie on youTube.
+ * This function updates the movie trailer by searching for an video official movie trailer of the given movie on youTube.
  * The function will take a movie title (keyword) and appends the phrase ' official trailer' to the search term.
- * The video's ID is stored and then used in the appropriate attributes (href, data-videoid, and src) in the iframe video on the index.
+ * The video's ID is stored and then used in the appropriate attributes (href, data-videoid, and src) in the iframe video on the index to allow the movie to play.
  * Note that this sample limits the results to 1.
- * @param
- * @return
+ * @param {string} keyword
  */
-function searchYouTubeByKeyword(keyword) {
+function updateMovieTrailerByKeyword(keyword) {
     keyword += ' official trailer';
     var result = null;
 
@@ -56,15 +52,9 @@ function searchYouTubeByKeyword(keyword) {
             var srcExtra = '?cc_load_policy=1&amp;controls=2&amp;rel=0&amp;hl=en&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fsupport.google.com&amp;widgetid=1';
 
             $('#youTubeVid').attr({'href': hrefMain + videoId, 'data-videoid': videoId, 'src': srcMain + videoId + srcExtra});
-            // $('#widget2').attr({'href': hrefMain + videoId, 'data-videoid': videoId, 'src': srcMain + videoId + srcExtra});
         },
         error: function (response) {
             console.log('what a failure?');
         }
     });
-    // var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-    // for(var i in results.items) {
-    //     var item = results.items[i];
-    //     console.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-    // }
 }
