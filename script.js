fb5b28094028223bd8fdc104ef78692134924343
Kyle Marx <Kyle Marx>
2016-09-21 15:43:42 -0700

    movie now loads to page based on ajax call and a hardcoded keyword

--- script.js
+++ script.js
@@ -1,67 +1,42 @@
 /**
  * global variables
  */
-var globalResult = null;
 
-/**
- * test of api call to flickr
- */
-
-var videoNumber = null;
 /**
  * document load function
  */
 $(document).ready(function () {
     addEventHandlers();
-    videoNumber = 0;
 });
 
 
+/**
+ *
+ */
 function addEventHandlers() {
     $('button').click(function () {
-        // searchByKeyword('terminator 2');
-
         updateVideo();
-        videoNumber++;
-        videoNumber %= 2;
     });
 }
 
 
+
 function updateVideo() {
-    var videoId = null;
-    if(videoNumber === 0){
-        videoId = 'lwSysg9o7wE';
-    }else {
-        videoId = 'knxhiwUspsA';
-    }
-    var hrefMain = '//www.youtube.com/watch?v=';
-    // var dataVideoIdMain = ''
-    var srcMain = 'https://www.youtube.com/embed/'
-    var srcExtra = '?cc_load_policy=1&amp;controls=2&amp;rel=0&amp;hl=en&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fsupport.google.com&amp;widgetid=1';
-
-    //href
-    //data-videoid
-    //src
-
-    $('#youTubeVid').attr({'href': hrefMain + videoId, 'data-videoid': videoId, 'src': srcMain + videoId + srcExtra});
-
+    searchYouTubeByKeyword('star wars episode vii');
 }
-
-// videos main link is    https://www.youtube.com/watch?v=
 
 
 /**
- * This function searches for a video with the given keyword and appends the word ' official trailer' to the search term.
- * The video's ID is
- * of the search results are logged to Apps Script's log.
- *
- * Note that this sample limits the results to 25. To return more results, pass
- * additional parameters as documented here:
- *   https://developers.google.com/youtube/v3/docs/search/list
+ * This function searches for an video official movie trailer for a given movie on youTube.
+ * The function will take a movie title (keyword) and appends the phrase ' official trailer' to the search term.
+ * The video's ID is stored and then used in the appropriate attributes (href, data-videoid, and src) in the iframe video on the index.
+ * Note that this sample limits the results to 1.
+ * @param
+ * @return
  */
-function searchByKeyword(keyword) {
-    keyword += ' trailer';
+function searchYouTubeByKeyword(keyword) {
+    keyword += ' official trailer';
+    var result = null;
 
     $.ajax({
         dataType: 'jsonp',
@@ -74,20 +49,19 @@ function searchByKeyword(keyword) {
             maxResults: 1
         },
         success: function (response) {
-            console.log('we got a movie');
+            // console.log('we got a movie');
             var videoId = response.items[0].id.videoId;
-            console.log(videoId);
-            var newPar = $('<p>').text(videoId);
-            $('body').append(newPar);
-            var fullAddress = 'https://www.youtube.com/watch?v=' + videoId;
-            console.log(fullAddress);
+            var hrefMain = '//www.youtube.com/watch?v=';
+            var srcMain = 'https://www.youtube.com/embed/';
+            var srcExtra = '?cc_load_policy=1&amp;controls=2&amp;rel=0&amp;hl=en&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fsupport.google.com&amp;widgetid=1';
+
+            $('#youTubeVid').attr({'href': hrefMain + videoId, 'data-videoid': videoId, 'src': srcMain + videoId + srcExtra});
+            // $('#widget2').attr({'href': hrefMain + videoId, 'data-videoid': videoId, 'src': srcMain + videoId + srcExtra});
         },
-        error: function () {
+        error: function (response) {
             console.log('what a failure?');
         }
-
     });
-
     // var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
     // for(var i in results.items) {
     //     var item = results.items[i];
